29cb82ca343801371620824510b10c6f7095355e
Simbala Diabira <simbaladiabira7136@gmail.com>
2023-09-16 22:31:07 +0000

    modification

--- script.js
+++ script.js
@@ -108,47 +108,6 @@ window.onscroll = ()=>{
 }
 
 // parallax ################
-
-
-// const observer = new IntersectionObserver((entries)=>{
-//     entries.forEach((entry)=>{
-//     if(entry.isIntersecting){
-//         entry.target.classList.add("show-items");
-//     }else{
-//         entry.target.classList.remove("show-items");
-//     });
-// });
-
-// const scrollScale = document.querySelectorAll(".scroll-scale");
-// scrollScale.forEach((el)=>observer(el));
-
-// const scrollScalBottom = document.querySelectorAll(".scroll-Bottom");
-// scrollBottom.forEach((el)=>observer(el));
-
-// const scrollScalTop = document.querySelectorAll(".scroll-Top");
-// scrollTop.forEach((el)=>observer(el));
-
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add("show-items");
-//         } else {
-//             entry.target.classList.remove("show-items");
-//         }
-//     });
-// });
-
-
-// const scrollScale = document.querySelectorAll(".scroll-scale");
-// scrollScale.forEach((el) => observer(el));
-
-// const scrollBottom = document.querySelectorAll(".scroll-Bottom"); // Correction du nom de variable
-// scrollBottom.forEach((el) => observer(el)); // Utilisation de scrollBottom
-
-// const scrollTop = document.querySelectorAll(".scroll-Top"); // Correction du nom de variable
-// scrollTop.forEach((el) => observer(el)); // Utilisation de scrollTop
-
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
